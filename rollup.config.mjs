d050c5e41e0ed73421193acbe811256c25d137d4
Hajsori <hajsori@proton.me>
2026-05-24 15:17:59 +0200

    Revert "first things to fix ws"
    This reverts commit f53b742c4b74d38baa36f25fc6cf02f5fdfb8293.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -35,7 +35,7 @@ const config = {
 			exportConditions: ["node"],
 			preferBuiltins: true
 		}),
-		commonjs({ ignoreDynamicRequires: true }),
+		commonjs(),
 		!isWatching && terser(),
 		{
 			name: "emit-module-package-file",
@@ -43,8 +43,7 @@ const config = {
 				this.emitFile({ fileName: "package.json", source: `{ "type": "module" }`, type: "asset" });
 			}
 		}
-	],
-	external: ["ws"]
+	]
 };
 
 export default config;
